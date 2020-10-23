54b9fb98fbaf196efd74051b432c90eb50646d62
hardik1729 <hardikjain3101@gmail.com>
2020-10-23 17:33:01 +0530

    first iteration for modulo

--- trial_old.cu
+++ trial_old.cu
@@ -1,7 +1,7 @@
 #include<iostream>
 #include<fstream>
 using namespace std;
-#define M (100+1)
+#define M (10000+1)
 #define N 2*(M-1)
 unsigned long long int B = 65535;
 unsigned long long int T = 1024;
@@ -15,6 +15,39 @@ __global__ void square(unsigned long long int I[M], unsigned long long int O[N])
 	unsigned long long int idx_start=(y+x*T);
 	unsigned long long int idx_end=(1+y+x*T);
 	long int C=B*T;
+
+	/*if(2*size-1>C){
+		int n=int((sqrtf(1+2*(2*size-1-C))-1)/2)+1;
+		int rem=2*size-1-C-2*n*(n-1);
+		int rem_l=rem/2;
+		int rem_r=rem-rem_l;
+		if(idx_start<n || idx_end>C-n){
+			if(idx_start<C/2){
+				int idx=idx_start;
+				idx_start=(n*n-(n-idx)*(n-idx));
+				idx_end=idx_start+(1+(n-idx-1)*2);
+			}else{
+				idx_start-=(C-n);
+				int idx=idx_start;
+				idx_start=(n*n+C-2*n+idx*idx);
+				idx_end=idx_start+(1+idx*2);
+			}
+		}else{
+			idx_start-=n;
+			idx_start+=(n*n);
+			idx_end-=n;
+			idx_end+=(n*n);
+		}	
+		if(idx_start==0){
+			idx_end+=rem_l;
+		}else if(idx_end==2*size-1-(rem_l+rem_r)){
+			idx_start+=rem_l;
+			idx_end+=(rem_r+rem_l);
+		}else{
+			idx_start+=rem_l;
+			idx_end+=rem_l;
+		}
+	}*/
 
 	for(unsigned long long int idx=idx_start+1;idx<idx_end+1;idx++){
 		if(idx<2*size){
@@ -39,6 +72,11 @@ __global__ void square(unsigned long long int I[M], unsigned long long int O[N])
 			}
 		}
 	}
+
+	//if(idx_end==2*size-1)
+		// printf("index_end : %d\n", idx_end);
+		// printf("%llu,%llu,%llu\n",B,I[0],T );
+	// O[N-1]=1;
 }
 
 int main(){
@@ -46,18 +84,16 @@ int main(){
 	unsigned long long int *hostI=new unsigned long long int[M];
 	unsigned long long int *hostO=new unsigned long long int[N];
 
-	unsigned long long int size=1;
+	int total_count=1;
+	int count=1;
+	unsigned long long int size=M-1;
 	unsigned long long int base=1024*1024;
-	unsigned long long int p;
-	cin>>p;
-	int total_count=p-2;
-	int count=0;
 
 	for(unsigned long long int i=0;i<size+1;i++){
 		if(i==0){
 			hostI[0]=size;
 		}else if(i==size){
-			hostI[i]=4;
+			hostI[i]=1;
 		}else{
 			hostI[i]=0;
 		}
@@ -71,12 +107,11 @@ int main(){
 
 	cudaMalloc((void**)&O, sizeof(unsigned long long int) * N);
 	while(count<=total_count){
-//SQUARE
 		cudaError_t err=cudaMemcpy(I,hostI,sizeof(unsigned long long int) * (size+1),cudaMemcpyHostToDevice);
-		cout<<cudaGetErrorString(err)<<endl;
+		cout<<err<<endl;
 
-		// err=cudaMemcpy(O,hostO,sizeof(unsigned long long int) * (2*size),cudaMemcpyHostToDevice);
-		// cout<<err<<endl;
+		err=cudaMemcpy(O,hostO,sizeof(unsigned long long int) * (2*size),cudaMemcpyHostToDevice);
+		cout<<err<<endl;
 
 		T=1024;
 		B=1+(2*size-1)/T;
@@ -88,78 +123,44 @@ int main(){
 
 		err=cudaMemcpy(hostO,O,sizeof(unsigned long long int) * 2*size,cudaMemcpyDeviceToHost);
 		cout<<cudaGetErrorString(err)<<endl;
-		// for(int i=1;i<2*size;i++)
-			// cout<<hostO[i]<<",";
-		// cout<<endl;
-
-//NORMALIZED SUM
-		int carry=0;
-		int idx;
-		for(idx=1;carry!=0 || idx<2*size;idx++){
-			hostI[idx]=(hostO[idx]+carry)%base;
-			carry=(carry+hostO[idx])/base;
-			// cout<<hostI[idx]<<",";	
-		}
-		// cout<<endl;
-		hostI[0]=--idx;
-
-//SUBTRACT 2
+		/*unsigned long long int c=0;
+		int pos=1;
 		int flag=0;
-		if(total_count!=count){
-			for(int i=1;i<hostI[0]+1;i++){
-				if(i==1){
-					if(hostI[i]<2){
-						flag=1;
-					}
-					hostI[i]=(hostI[i]+base-2)%base;
-				}else if(flag){
-					if(hostI[i]<1)
-						flag=1;
-					else
-						flag=0;
-					hostI[i]=(hostI[i]+base-1)%base;
+		while (c!=0 || pos<2*size){
+			if(pos>=2*size)
+				hostO[pos]=0;
+			hostO[pos]=hostO[pos]+c;
+			c=hostO[pos]/base;
+			hostO[pos]=hostO[pos]%base;
+			if(pos==1){
+				if(hostO[pos]<2){
+					hostO[pos]=base-2+hostO[pos];
+					flag=1;
 				}else{
-					break;
+					hostO[pos]-=2;
 				}
-				// cout<<hostI[i]<<",";
+			}else if(flag==1 && hostO[pos]==0){
+				hostO[pos]=base-1;
+			}else if(flag==1){
+				hostO[pos]-=1;
+				flag=0;
 			}
-			// cout<<endl;
-			if(hostI[idx]==0)
-				hostI[0]--;
+			hostI[pos]=hostO[pos];
+			// cout<<hostO[pos]<<",";
+			hostO[pos]=0;
+			if((pos>=2*size && (c!=0 || hostI[pos]!=0)) || pos<2*size)
+				pos++;
 		}
-
-//MODULO REDUCTION TO HALF
-		while(hostI[0]>p/20+1){
-			flag=0;
-			if(2*(p/20+1)-hostI[0]==1)
-				flag=1;
-			carry=0;
-			for(idx=1;carry!=0 || (idx<=hostI[0] && idx<=p/20+1);idx++){
-				if(idx>p/20+1){
-					hostI[idx]=carry;
-					carry=0;
-				}else if(idx==p/20+1 && flag){
-					hostI[idx]=(hostI[idx]+carry);
-					carry=(hostI[idx])/base;
-					hostI[idx]%=base;		
-				}else{
-					hostI[idx]=(hostI[idx]+hostI[idx+p/20+1]*pow(2,20-p%20)+carry);
-					carry=(hostI[idx])/base;
-					hostI[idx]%=base;
-				}
-				cout<<idx<<","<<hostI[idx]<<","<<carry<<";";
-			}
-			cout<<endl;
-			while(--idx>-1 && hostI[idx]==0);
-			hostI[0]=idx;
+		if(hostI[pos-1]==0){
+			pos--;
 		}
-
-//MODULO
-		// while(hostI[0]==p/20+1 && hostI[hostI[0]]>=pow(2,p%20)){
-		// 	cout<<endl<<"comeback after coding it.";
-		// }
- 		size=hostI[0];
-		cout<<"size : "<<hostI[0]<<endl<<endl;
+		hostO[0]=pos-1;
+		hostI[0]=hostO[0];
+		size=hostI[0];
+		hostO[0]=0;*/
+		cout<<"size : "<<hostI[0]<<","<<hostO[2*size-1]<<endl;
+	//for(int i=0;i<2*size;i++)
+		//cout<<"working : "<<hostO[i]<<",";
 		count++;
 	}
 	return 0;
