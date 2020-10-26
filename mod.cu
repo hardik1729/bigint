502b9eed4dc174580064f82d56aa2209f0dc8581
hardik1729 <hardikjain3101@gmail.com>
2020-10-26 15:53:20 +0530

    COMPLETE

--- mod.cu
+++ mod.cu
@@ -76,9 +76,6 @@ int main(){
 		cudaError_t err=cudaMemcpy(I,hostI,sizeof(unsigned long long int) * (size+1),cudaMemcpyHostToDevice);
 		cout<<cudaGetErrorString(err)<<endl;
 
-		// err=cudaMemcpy(O,hostO,sizeof(unsigned long long int) * (2*size),cudaMemcpyHostToDevice);
-		// cout<<err<<endl;
-
 		T=1024;
 		B=1+(2*size-1)/T;
 
@@ -89,12 +86,9 @@ int main(){
 
 		err=cudaMemcpy(hostO,O,sizeof(unsigned long long int) * 2*size,cudaMemcpyDeviceToHost);
 		cout<<cudaGetErrorString(err)<<endl;
-		// for(int i=1;i<2*size;i++)
-			// cout<<hostO[i]<<",";
-		// cout<<endl;
 
 //NORMALIZED SUM
-		int carry=0;
+		unsigned long long int carry=0;
 		int idx;
 		for(idx=1;carry!=0 || idx<2*size;idx++){
 			hostI[idx]=(hostO[idx]+carry)%base;
@@ -105,8 +99,8 @@ int main(){
 		hostI[0]=--idx;
 
 //SUBTRACT 2
-		int flag=0;
 		if(total_count!=count){
+			int flag=0;
 			for(int i=1;i<hostI[0]+1;i++){
 				if(i==1){
 					if(hostI[i]<2){
@@ -129,44 +123,49 @@ int main(){
 				hostI[0]--;
 		}
 
-//MODULO REDUCTION TO HALF
+//MODULO
 		cout<<"step : "<<count<<endl;
-		int m_count=0;
-		while(hostI[0]>p/s+1 && m_count<5){
-			flag=0;
-			if(2*(p/s+1)-hostI[0]==1)
-				flag=1;
-			carry=0;
-			for(idx=1;carry!=0 || (idx<=hostI[0] && idx<=p/20+1);idx++){
+		while(hostI[0]>p/s+1){
+			for(idx=1;carry!=0 || idx<=p/s+1;idx++){
 				if(idx>p/s+1){
 					hostI[idx]=carry;
-					carry=0;
-				}else if(idx==p/s+1 && flag){
-					hostI[idx]=(hostI[idx]+carry);
-					carry=(hostI[idx])/base;
-					hostI[idx]%=base;		
+					carry=hostI[idx]/base;
+					hostI[idx]%=base;
+				}else if(idx+p/s+1>hostI[0]){
+					hostI[idx]=hostI[idx]+carry;
+					carry=hostI[idx]/base;
+					hostI[idx]%=base;
 				}else{
-					hostI[idx]=(hostI[idx]+hostI[idx+p/s+1]*pow(2,s-p%s)+carry);
-					carry=(hostI[idx])/base;
+					hostI[idx]=hostI[idx]+hostI[idx+p/s+1]*pow(2,s-p%s)+carry;
+					carry=hostI[idx]/base;
 					hostI[idx]%=base;
 				}
-				// cout<<idx<<","<<hostI[idx]<<";";
+				// cout<<idx<<","<<hostI[idx]<<","<<carry<<";";
 			}
 			// cout<<endl;
 			while(--idx>-1 && hostI[idx]==0);
 			hostI[0]=idx;
-			m_count++;
 		}
 
-//MODULO
-		// while(hostI[0]==p/20+1 && hostI[hostI[0]]>=pow(2,p%20)){
-		// 	cout<<endl<<"comeback after coding it.";
-		// }
  		size=hostI[0];
- 		for(int i=1;i<size+1;i++)
- 			cout<<i-1<<","<<hostI[i]<<";";
- 		cout<<endl;
-		cout<<"size : "<<hostI[0]<<endl<<endl;
+ 		if(total_count==count && size==p/s+1){
+ 			int flag=1;
+ 			for(int i=1;i<size+1;i++){
+ 				if(i>1 && i<size)
+ 					flag=flag && (hostI[i]==base-1);
+ 				cout<<i-1<<","<<hostI[i]<<";";
+ 			}
+ 			cout<<endl;
+ 			unsigned long long int l=base-hostI[1];
+ 			unsigned long long int h=pow(2,p%s)*l-1;
+ 			if(h==hostI[size] && flag)
+ 				cout<<"PRIME"<<endl;
+ 			else
+ 				cout<<"COMPOSITE"<<endl;
+ 		}else{
+ 			cout<<"TRICKY"<<endl;
+ 		}
+ 		cout<<"size : "<<hostI[0]<<endl<<endl;
 		count++;
 	}
 	return 0;
